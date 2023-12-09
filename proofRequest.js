a46c12918baf235f183ca39a7aaea5256aa42e67
0xblackdev <0xdev@0xblackdevs-MacBook-Pro.local>
2023-12-09 15:21:33 +0530

    temp commit

--- proofRequest.js
+++ proofRequest.js
@@ -1,17 +1,8 @@
 const { KYCAgeCredential } = require("./vcHelpers/KYCAgeCredential");
 
-// design your own customised authentication requirement here using Query Language
-// https://0xpolygonid.github.io/tutorials/verifier/verification-library/zk-query-language/
+const humanReadableAuthReason = "Must vote coz its your right";
 
-const humanReadableAuthReason = "Must be born before this year";
-
-const credentialSubject = {
-  birthday: {
-    // users must be born before this year
-    // birthday is less than Jan 1, 2023
-    $lt: 20230101,
-  },
-};
+const credentialSubject = {};
 
 const proofRequest = KYCAgeCredential(credentialSubject);
 
